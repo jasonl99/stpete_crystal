747bd65a08af5ca4e56178e40d89864a208b7d1f
Jason Landry <jasonl99@gmail.com>
2017-01-16 22:22:25 -0500

    added websocket update capability

--- src/stpete_crystal.cr
+++ src/stpete_crystal.cr
@@ -6,12 +6,22 @@ require "kilt/slang"
 module StpeteCrystal
 
   class_property visits = 0
+  VISIT_SOCKETS = [] of HTTP::WebSocket
 
   Session.config do |config|
     config.timeout = 2.minutes
     config.cookie_name = "session_id"
     config.secret = "some_secret"
     config.gc_interval = 2.minutes
+  end
+
+  ws "/global_visits" do |socket|
+
+   VISIT_SOCKETS << socket
+   socket.on_close do
+     VISIT_SOCKETS.delete socket
+   end
+
   end
 
   get "/hello" do |context|
@@ -26,6 +36,11 @@ module StpeteCrystal
   # updates the current visitor's session, including the name.
   # This exploits one of crystal's features that's not
   # available in ruby: method overloads.
+  def self.visits=( visit_count : Int32 )
+    @@visits = visit_count
+    VISIT_SOCKETS.each {|socket| socket.send visit_count.to_s}
+  end
+
   def self.update_session( user_session : Session, visitor_name : String)
     user_session.string("name", visitor_name)
     update_session(user_session)
