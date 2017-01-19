fff1d15b17005a3131554e7f482f73a594f62c48
Jason Landry <jasonl99@gmail.com>
2017-01-19 14:47:22 -0500

    hello_jason_web

--- src/stpete_crystal.cr
+++ src/stpete_crystal.cr
@@ -4,6 +4,12 @@ module StpeteCrystal
   get "/hello" do |context|
     "Hello, web!"
   end
+
+  get "/hello/:name" do |context|
+    name = context.params.url["name"]
+    "Hello, #{name.capitalize}!"
+  end
+
   Kemal.run
 
 end
